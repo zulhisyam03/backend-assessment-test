762b1cf13a2d9572af1af0342abf86d05f185bf9
zulhisyam03 <zul0342@gmail.com>
2025-08-11 22:18:23 +0800

    Passed - customer cannot see a list of debit cards of other customers

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -61,6 +61,32 @@ class DebitCardControllerTest extends TestCase
     public function testCustomerCannotSeeAListOfDebitCardsOfOtherCustomers()
     {
         // get /debit-cards
+        // Buat debit card milik user lain
+        DebitCard::factory()->count(2)->create([
+            'user_id' => User::factory()->create()->id,
+            'disabled_at' => null,
+            'number' => fn() => rand(1_000_000_000, 2_147_483_647),
+        ]);
+
+        // Buat debit card milik user ini
+        $cardsUser = DebitCard::factory()->count(1)->create([
+            'user_id' => $this->user->id,
+            'disabled_at' => null,
+            'number' => fn() => rand(1_000_000_000, 2_147_483_647),
+        ]);
+
+        $response = $this->getJson('/api/debit-cards');
+
+        $response->assertStatus(200);
+
+        $json = $response->json();
+        $cards = $json['data'] ?? $json;
+
+        // Pastikan hanya 1 kartu yang tampil (milik user ini)
+        $this->assertCount(1, $cards);
+
+        // Pastikan kartu yang tampil adalah kartu milik user ini
+        $this->assertEquals($cardsUser->first()->id, $cards[0]['id']);
     }
 
     public function testCustomerCanCreateADebitCard()
